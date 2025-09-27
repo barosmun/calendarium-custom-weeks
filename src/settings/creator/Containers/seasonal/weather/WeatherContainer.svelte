ece188ffe0f6c36fe6bd18bd9265041291b0f77e
ZoetjeZuurtje <85112688+ZoetjeZuurtje@users.noreply.github.com>
2025-09-27 02:42:00 +0200

    Freezing temperature (#207)
    * feat: Allow user to set freezing temperature in settings
    
    * fix: Support both unitsystems
    
    * fix: Added default values
    
    ---------
    
    Co-authored-by: = <=>

--- src/settings/creator/Containers/seasonal/weather/WeatherContainer.svelte
+++ src/settings/creator/Containers/seasonal/weather/WeatherContainer.svelte
@@ -6,15 +6,18 @@
     import TextComponent from "src/settings/creator/Settings/TextComponent.svelte";
     import ToggleComponent from "src/settings/creator/Settings/ToggleComponent.svelte";
     import Details from "src/settings/creator/Utilities/Details.svelte";
-    import { getWeatherSeed } from "src/utils/functions";
+    import { getWeatherSeed, translateTemperature } from "src/utils/functions";
     import { getContext } from "svelte";
     const calendar = getContext("store");
     const { weatherStore } = calendar;
-    const { enabled, seed, tempUnitsStore, windUnitsStore } = weatherStore;
+    const { enabled, seed, tempUnitsStore, windUnitsStore, freezingPointStore } = weatherStore;
 
     const newSeed = (node: HTMLElement) => {
         new ExtraButtonComponent(node).setIcon("rotate-ccw");
     };
+    
+    $: usesFahrenheit = $tempUnitsStore === UnitSystem.IMPERIAL;
+    $: shownFreezingTemperature = usesFahrenheit ? translateTemperature($freezingPointStore, "Imperial", "Metric") : $freezingPointStore;
 </script>
 
 <Details
@@ -50,6 +53,23 @@
             ></div>
         </TextComponent>
 
+        <div class="setting-item">
+            <SettingItem>
+                <div slot="name">Freezing temperature</div>
+                <div slot="desc">Sets the temperature at which rain turns into snow. Default is 0 for Celsius, and 32 for Fahrenheit.</div>
+                <input
+                    slot="control"
+                    type="number"
+                    bind:value={shownFreezingTemperature}
+                    on:change={(e) => {
+                            const temperature = Number(e?.currentTarget.value ?? 0);
+                            const tempInCelsius = usesFahrenheit ? translateTemperature(temperature, "Metric", "Imperial") : temperature;
+                            $freezingPointStore = tempInCelsius;
+                        }
+                    }
+                />
+            </SettingItem>
+        </div>
         <div class="setting-item">
             <SettingItem>
                 <div slot="name">Temperature units</div>
